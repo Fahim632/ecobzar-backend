1142d6c085e2f06eb8cb7a99f779299b438f6710
Fahim632 <117130476+Fahim632@users.noreply.github.com>
2026-05-05 18:38:33 +0600

    db connect

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hlw")
+require('node:dns/promises').setServers(["1.1.1.1", "8.8.8.8"])
+
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors")
+const dbConfig = require("./config/dbConfig");
+
+const app = express();
+// middlewore
+app.use(express.json());
+app.use(cors());
+
+// database
+dbConfig();
+
+app.get('/',(req,res)=>{
+    res.send("hlw")
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`servar running on port ${port}`);
+})
